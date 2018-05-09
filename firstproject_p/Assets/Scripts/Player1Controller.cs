20fff761563084c1d62c8908327db5f6931e7bd4
pguenes <palomaguenes@gmail.com>
2018-05-09 13:28:48 +0300

    Roll a ball: https://unity3d.com/learn/tutorials/s/roll-ball-tutorial

---
+++ firstproject_p/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1Controller : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 9) {
+			winText.text = "You win!";
+		}
+	}
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+
+		
+}
